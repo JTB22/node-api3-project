6df7978fb8de3ac4c9efb6d56fa6d79c4d8c179c
Josh Blood <jblood92@pm.me>
2023-09-08 13:07:04 -0400

    MVP

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,3 +1,6 @@
+const Users = require("../users/users-model");
+const Posts = require("../posts/posts-model");
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
   const { method, originalUrl } = req;
@@ -6,16 +9,42 @@ function logger(req, res, next) {
   next();
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  const { id } = req.params;
+  try {
+    const user = await Users.getById(id);
+    if (!user) {
+      res.status(404).json({ message: "user not found" });
+    } else {
+      req.user = user;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
 }
 
-function validateUser(req, res, next) {
+async function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  const { name } = req.body;
+  if (!name || !name.trim()) {
+    res.status(400).json({ message: "missing required name field" });
+  } else {
+    req.name = name.trim();
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  const { text } = req.body;
+  if (!text || !text.trim()) {
+    res.status(400).json({ message: "missing required text field" });
+  } else {
+    req.text = text.trim();
+    next();
+  }
 }
 
 // do not forget to expose these functions to other modules
